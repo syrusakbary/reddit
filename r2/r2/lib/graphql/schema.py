17f8e08b7cc06d5c7782ac619010f9407d20a350
Syrus Akbary <me@syrusakbary.com>
2016-12-18 22:43:09 -0800

    Added front, random and all fake subreddits

--- r2/r2/lib/graphql/schema.py
+++ r2/r2/lib/graphql/schema.py
@@ -4,13 +4,27 @@ from r2.lib.graphql.user import User
 from r2.lib.graphql.subreddit import Subreddit
 from r2.lib.graphql.node import Node
 
+from r2.models.subreddit import Frontpage, All, Random
+
 class Query(graphene.ObjectType):
 
     viewer = graphene.Field(User)
+    front = graphene.Field(Subreddit)
+    all = graphene.Field(Subreddit)
+    random = graphene.Field(Subreddit)
     node = Node.Field()
 
     def resolve_viewer(self, args, context, info):
         return context.user
+
+    def resolve_front(self, args, context, info):
+        return Frontpage
+
+    def resolve_all(self, args, context, info):
+        return All
+
+    def resolve_random(self, args, context, info):
+        return Random
 
 
 schema = graphene.Schema(query=Query, types=[Subreddit])
--- r2/r2/lib/graphql/subreddit.py
+++ r2/r2/lib/graphql/subreddit.py
@@ -1,6 +1,6 @@
 import graphene
 
-from r2.models.subreddit import Subreddit as SubredditModel
+from r2.models.subreddit import Subreddit as SubredditModel, FakeSubreddit
 from r2.lib.graphql.node import Node
 from r2.lib.graphql.user import User
 
@@ -25,10 +25,11 @@ class Subreddit(graphene.ObjectType):
 
     @staticmethod
     def is_type_of(root, context, info):
-        return isinstance(root, SubredditModel)
+        return isinstance(root, (SubredditModel, FakeSubreddit))
 
     def resolve_id(self, args, context, info):
         return self._fullname
 
     def resolve_moderators(self, args, context, info):
-        return map(Account._byID, self.moderators)
+        moderators = getattr(self, 'moderators', [])
+        return map(Account._byID, moderators)
